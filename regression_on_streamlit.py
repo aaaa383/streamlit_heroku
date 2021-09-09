041f95ceb9e8410d302cdea9187c19034756a58a
aaaa383 <311.1999.kenta@gmail.com>
2021-09-10 00:00:04 +0900

    sample

--- regression_on_streamlit.py
+++ regression_on_streamlit.py
@@ -12,27 +12,24 @@ from sklearn.metrics import r2_score
 sns.set()
 
 
-# Display a title
-st.title('Linear regression on Boston house prices')
+
+st.title('ボストン市の住宅価格の重回帰分析')
 
 
-# load the dataeset
+# データの読み込み
 dataset = load_boston()
-# Create the explanatory variables as DataFrame in pandas
 df = pd.DataFrame(dataset.data)
-# Assign the variable names as column name
 df.columns = dataset.feature_names
-# Create the target-variable name as "PRICES"
 df["PRICES"] = dataset.target
 
 
-# Display dataset when check box is ON
-if st.checkbox('View dataset in table data format'):
+# チェックボックスがONの時、データセットを表示
+if st.checkbox('テーブル形式でデータセットを表示'):
   st.dataframe(df)
 
 
-# Show each column description when checkbox is ON.
-if st.checkbox('Show each column name and its description'):
+# チェックボックスがONの時、
+if st.checkbox('カラム名とその説明'):
   st.markdown(
         r"""
         ### Column name and its Description
@@ -54,15 +51,12 @@ if st.checkbox('Show each column name and its description'):
         )
 
 
-# Plot the relation between target and explanatory variables
-# when the checkbox is ON.
-if st.checkbox('Plot the relation between target and explanatory variables'):
-  # Select one explanatory variable for ploting
+#チェック時に目的変数と説明変数の相関を可視化
+if st.checkbox('相関係数の可視化'):
   checked_variable = st.selectbox(
-    'Select one explanatory variable:',
+    '説明変数を1つ選択してください:',
     df.drop(columns="PRICES").columns
     )
-  # Plot
   fig, ax = plt.subplots(figsize=(5, 3))
   ax.scatter(x=df[checked_variable], y=df["PRICES"])
   plt.xlabel(checked_variable)
@@ -71,84 +65,76 @@ if st.checkbox('Plot the relation between target and explanatory variables'):
 
 
 """
-## Preprocessing
+## 前処理
 """
 
-# Select the variables you will NOT use
 Features_chosen = []
 Features_NonUsed = st.multiselect(
-  'Select the variables you will NOT use', 
+  '学習時に使用しない変数を選択してください',
   df.drop(columns="PRICES").columns
   )
 
-# Drop the columns you selected
 df = df.drop(columns=Features_NonUsed)
 
 
-# Choose whether you will perform logarithmic transformation
+#対数変換の実施有無を選択
 left_column, right_column = st.beta_columns(2)
 bool_log = left_column.radio(
-      'You will perform logarithmic transformation?', 
+      '対数変換を行いますか？',
       ('No','Yes')
       )
 
 df_log, Log_Features = df.copy(), []
 if bool_log == 'Yes':
   Log_Features = right_column.multiselect(
-          'Select the variables you will perform logarithmic transformation',
+          '対数変換を適用する目的変数もしくは説明変数を選択してください',
           df.columns
           )
-  # Perform the lagarithmic transformation
   df_log[Log_Features] = np.log(df_log[Log_Features])
 
 
-# Choose whether you will perform standardization
+#標準化の実施有無を選択
 left_column, right_column = st.beta_columns(2)
 bool_std = left_column.radio(
-      'You will perform standardization?',
+      '標準化を実施しますか？',
       ('No','Yes')
       )
 
 df_std = df_log.copy()
 if bool_std == 'Yes':
   Std_Features_NotUsed = right_column.multiselect(
-          'Select the variables you will NOT perform standardization', 
+          '標準化を適用しない説明変数を選択してください',
           df_log.drop(columns=["PRICES"]).columns
           )
-  # Assign the explanatory variables, 
-  # excluded of ones in "Std_Features_NotUsed",
-  # to "Std_Features_chosen"
   Std_Features_chosen = []
   for name in df_log.drop(columns=["PRICES"]).columns:
     if name in Std_Features_NotUsed:
       continue
     else:
       Std_Features_chosen.append(name)
-  # Perform standardization
   sscaler = preprocessing.StandardScaler()
   sscaler.fit(df_std[Std_Features_chosen])
   df_std[Std_Features_chosen] = sscaler.transform(df_std[Std_Features_chosen])
 
 
 """
-### Split the dataset into training and validation datasets
+### データセットを訓練用と検証用に分割
 """
 left_column, right_column = st.beta_columns(2)
 test_size = left_column.number_input(
-        'Validation data size(rate: 0.0-1.0):',
+        '検証用データのサイズ(rate: 0.0-1.0):',
         min_value=0.0,
         max_value=1.0,
         value=0.2,
         step=0.1,
          )
 random_seed = right_column.number_input(
-              'Random seed(Nonnegative integer):',
+              'ランダムシードの設定(Nonnegative integer):',
                 value=0, 
                 step=1,
                 min_value=0)
 
 
-# Split the dataset
 X_train, X_val, Y_train, Y_val = train_test_split(
   df_std.drop(columns=["PRICES"]), 
   df_std['PRICES'], 
@@ -157,14 +143,12 @@ X_train, X_val, Y_train, Y_val = train_test_split(
   )
 
 
-# Create an instance of liner regression
 regressor = LinearRegression()
 regressor.fit(X_train, Y_train)
 
 Y_pred_train = regressor.predict(X_train)
 Y_pred_val = regressor.predict(X_val)
 
-# Perform inverse conversion if the logarithmic transformation was performed.
 if "PRICES" in Log_Features:
   Y_pred_train, Y_pred_val = np.exp(Y_pred_train), np.exp(Y_pred_val)
   Y_train, Y_val = np.exp(Y_train), np.exp(Y_val)
@@ -172,38 +156,35 @@ if "PRICES" in Log_Features:
 
 
 """
-## Show the results
+## 結果の表示
 """
 
 """
-### Accuracy of the model
+### モデル精度
 """
 R2 = r2_score(Y_val, Y_pred_val)
 st.write(f'R2 value: {R2:.2f}')
 
 
 """
-### Plot the results
+### グラフ描画
 """
 left_column, right_column = st.beta_columns(2)
 show_train = left_column.radio(
-        'Plot the result of the training dataset:', 
+        '訓練データの結果を表示:',
         ('Yes','No')
         )
 show_val = right_column.radio(
-        'Plot the result of the validation dataset:', 
+        '検証データの表示:',
         ('Yes','No')
         )
 
 
-# Get the maximum value of all objective variable data,
-# including predicted values
 y_max_train = max([max(Y_train), max(Y_pred_train)])
 y_max_val = max([max(Y_val), max(Y_pred_val)])
 y_max = int(max([y_max_train, y_max_val])) 
 
 
-# Allows the axis range to be changed dynamically
 left_column, right_column = st.beta_columns(2)
 x_min = left_column.number_input('x_min:',value=0,step=1)
 x_max = right_column.number_input('x_max:',value=y_max,step=1)
@@ -212,7 +193,6 @@ y_min = left_column.number_input('y_min:',value=0,step=1)
 y_max = right_column.number_input('y_max:',value=y_max,step=1)
 
 
-# Show the results
 fig = plt.figure(figsize=(3, 3))
 if show_train == 'Yes':
   plt.scatter(Y_train, Y_pred_train,lw=0.1,color="r",label="training data")
@@ -225,7 +205,6 @@ plt.ylim(int(y_min), int(y_max)+5)
 plt.legend(fontsize=6)
 plt.tick_params(labelsize=6)
 
-# Display by Streamlit
 st.pyplot(fig)
 
 
